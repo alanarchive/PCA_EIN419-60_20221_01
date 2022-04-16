2219719fbd58f8c4768db53c0c48826e44078b31
alanarchive <61806343+alanarchive@users.noreply.github.com>
2022-04-15 22:29:50 -0300

    Atualização_15_04_22

--- PCA_EIN419-60_20221_01.py
+++ PCA_EIN419-60_20221_01.py
@@ -1,9 +1,10 @@
 import random
 from tkinter import *
-from  pygame import mixer
+from pygame import mixer
 
 mixer.init()
 mixer.music.load('fun-kids-playful.mp3')
+mixer.music.set_volume(0.3)
 mixer.music.play(-1)
 
 
@@ -12,24 +13,29 @@ num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 def submt(var1):
     if var1.get() == str(resultPLUS()):
-        correct = Label(app, text="Acertou!", fg="green", font=("Arial", 36, "bold"))
+        correct = Label(app, text="Acertou!", fg="green", bg="#edc5e4", font=("Arial", 36, "bold"))
         correct.place(relx=0.20, rely=0.15)
+        mixer.music.load('ok.mp3')
+        mixer.music.play(0)
     else:
-        wrong = Label(app, text="Errou!", fg="red", font=("Arial", 36, "bold"))
+        wrong = Label(app, text="Errou!", fg="red", bg="#edc5e4", font=("Arial", 36, "bold"))
         wrong.place(relx=0.29, rely=0.15)
-
+        mixer.music.load('no.mp3')
+        mixer.music.play(0)
 
 def try_again():
     try_again.num1update = random.choice(num)
     try_again.num2update = random.choice(num)
     newQ = Label(
-        app, text=f"{try_again.num1update}+{try_again.num2update}",background="#edc5e4", font=("Arial", 36, "bold")
+        app, text=f"{try_again.num1update}+{try_again.num2update}", background="#edc5e4", font=("Arial", 36, "bold")
     )
     newQ.place(relx=0.16, rely=0.14, relwidth=0.7, relheight=0.23)
-
+    mixer.music.load('fun-kids-playful.mp3')
+    mixer.music.play(0)
 
 def resultPLUS():
     try_again
+    solving.delete(0, END)
     return try_again.num1update + try_again.num2update
 
 
@@ -42,10 +48,13 @@ app.configure(bg='#edc5e4')
 app.resizable(False, False)
 start = Button(app, text="START", command=try_again, font='bold', width=10, foreground="white", bg="#db3db6")
 start.place(relx=0.35, rely=0.25)
-solving = Entry(app, font=("Arial", 36, "bold", ),foreground="white", bg="#db3db6")
+solving = Entry(app, font=("Arial", 36, "bold", ), foreground="white", bg="#db3db6")
 solving.place(relx=0.35, rely=0.4, relwidth=0.34, relheight=0.23)
 submit = Button(app, text="VERIFICAR", font='bold', command=lambda: submt(solving),foreground="white", bg="#db3db6")
 submit.place_configure(relx=0.35, rely=0.67, relwidth=0.34, relheight=0.1)
 try_again = Button(app, text="TENTE NOVAMENTE!",font='bold', command=try_again, foreground="white", bg="#db3db6")
 try_again.place(relx=0.24, rely=0.83,relwidth=0.57, relheight=0.1 )
 app.mainloop()
+
+
+
